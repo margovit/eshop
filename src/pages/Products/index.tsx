049908ed9687df70e3521d6ab463dd427eaec57e
Marharyta Vitlytska <vitlytskam@malfini.com>
2024-03-28 13:25:11 +0100

    New logo

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -40,47 +40,48 @@ export function Products() {
 export function ProductCard({ item }: { item: ProductDto }) {
     return (
         <div className="flex flex-wrap justify">
+            <div className="card-wrapper">
             <Card className="bg-dimmed w-64 h-64" shadow="sm" padding="lg" radius="md" withBorder>
-                    <Card.Section withBorder inheritPadding py="xs">
+                <Card.Section withBorder inheritPadding py="xs">
                     <Image className="w-20 h-20" src={item.image} alt={item.title} />
-                        <Group justify="space-between" >
-                            <Text fz="sm" fw={500}>
-                                {item.title}
-                            </Text>
-                            <Group gap={5}>
-                                <Text fz="xs" c="dimmed">
-                                    {item.description}
-                                </Text>
-                            </Group>
-                        </Group>
-                        <Text mt="sm" mb="md" c="dimmed" fz="xs">
-                            {item.category}
+                    <Group justify="space-between" >
+                        <Text fz="sm" fw={500}>
+                            {item.title}
                         </Text>
-                        <Card.Section>
-                            <Text size="xs" color="dimmed">
-                                Product Information:
-                            </Text>
-                            <Text fw={500} size="sm">
+                        <Group gap={5}>
+                            <Text fz="xs" c="dimmed">
                                 {item.description}
                             </Text>
-                            <Text fw={500} size="sm">
-                                Price: {item.price}
-                            </Text>
-                            <Button variant="filled" color="gray">Button</Button>
-                        </Card.Section>
+                        </Group>
+                    </Group>
+                    <Text mt="sm" mb="md" c="dimmed" fz="xs">
+                        {item.category}
+                    </Text>
+                    <Card.Section>
+                        <Text size="xs" color="dimmed">
+                            Product Information:
+                        </Text>
+                        <Text fw={500} size="sm">
+                            {item.description}
+                        </Text>
+                        <Text fw={500} size="sm">
+                            Price: {item.price}
+                        </Text>
+                        <Button variant="filled" color="gray" size="xs" radius="lg">Buy</Button>
                     </Card.Section>
-                
+                </Card.Section>
             </Card>
-            </div>
-            );
+        </div>
+    </div >      
+    );
 }
 
-            export default function ProductCardList({product: productList }: {product: ProductDto[] }) {
+export default function ProductCardList({ product: productList }: { product: ProductDto[] }) {
     return (
-            <ul className="divide-y divide-gray-200">
-                {productList.map((productItem) => (
-                    <ProductCard key={productItem.id} item={productItem} />
-                ))}
-            </ul>
-            );
+        <ul className="divide-y divide-gray-200">
+            {productList.map((productItem) => (
+                <ProductCard key={productItem.id} item={productItem} />
+            ))}
+        </ul>
+    );
 }
